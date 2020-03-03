0143f8f6d1353470ec57cb6bcaa6701f2753830f
mekchong <chongyizheng99@gmail.com>
2020-03-02 22:17:56 -0600

    shud be done

--- src/code/TicketMachine.java
+++ src/code/TicketMachine.java
@@ -8,8 +8,11 @@ public class TicketMachine {
 	private int cost;
 	private int total;
 	private int currentTicketAmount;
-	private int ticketCount;				// cost of one regular ticket
-	private int leftToPay;
+	private int ticketCount;
+	private int FCTicketCount;
+	private int FCtotal;
+	private int amountRequired;
+	private int refundAmount;
 
 	// constructors
 	// -- default constructor
@@ -21,7 +24,7 @@ public class TicketMachine {
 	public TicketMachine (int cost) {
 		this.cost = cost;
 	}
-	
+
 	// other methods besides constructors
 	// -- getter for cost
 	public int getCost () {
@@ -35,44 +38,81 @@ public class TicketMachine {
 			return currentTicketAmount;
 		}
 		else{
+			total+=received;
 			return currentTicketAmount += received;
 		}
+	}
+
+	//printing a regular ticket
+	public String printTicket(){
+		//checking for sufficient funds
+		if(cost > currentTicketAmount){
+			String err = "Insufficient funds";
+			amountRequired = balanceAmount(currentTicketAmount, cost);
+			return err;
+		}
+		else{
+			if(currentTicketAmount > cost){
+				//returning balance after printing ticket
+				refundAmount = balanceAmount(cost, currentTicketAmount);
+			}
+			ticketCount++;
+			String ticket = "This is your regular ticket";
+			return ticket;
+		}
+
+	}
+
+	//buying a first class ticket
+	public void firstClass(){
+		if(currentTicketAmount>=cost*2){
+			currentTicketAmount-=cost*2;
+			FCtotal += cost*2;
+			FCTicketCount++;
+			System.out.println("This is your first class ticket");
+		} else {
+			System.out.println("Insufficient funds for first class ticket. $" + balanceAmount(cost*2, currentTicketAmount) + " required");
+		}
+	}
+
+	//calculating amount to refund/left to pay
+	public int balanceAmount(int val1, int val2){
+		return val2 - val1;
+	}
+
+	//getter methods
+	public int getTotalMoney(){
+		System.out.println("Total money collected: $" + total);
+		return total;
 	}
 
 	// getter for amount of money collected for a ticket
 	public int getCurrentCollected(){
 		return currentTicketAmount;
 	}
-
-	public int totalMoney(){
-		total += currentTicketAmount;
-		System.out.println("Total money collected: $" + total);
-		return total;
+	//gets total number of tickets for both first class and regular tickets
+	public int getGrandTicketCount(){
+		return (ticketCount + FCTicketCount);
 	}
 
+	//gets total number of tickets for first class tickets
+	public int getFCTicketCount(){
+		return FCTicketCount;
+	}
+
+	//gets the amount needed to complete a ticket
+	public int getAmountRequired(){
+		return amountRequired;
+	}
+
+	//gets the amount of money needed to complete a ticket
+	public int getRefundAmount(){
+		return refundAmount;
+	}
+
+	//gets the number of of regular tickets sold
 	public int getTicketCount(){
 		return ticketCount;
 	}
-
-	public void sufficientFundsCheck(){
-		if(currentTicketAmount < cost){
-			leftToPay = cost - currentTicketAmount;
-			System.out.println("Insufficient funds. $" + leftToPay+ "more required");
-		}
-		else{
-			printTicket();
-		}
-	}
-
-	public String printTicket(){
-		/*
-		ticketCount++;
-		total += currentTicketAmount;
-		currentTicketAmount = 0;
-		*/
-		String ticket = "This is your ticket";
-		return ticket;
-	}
-
 
 }	// end - class TicketMachine
--- src/tests/TestTicketMachine.java
+++ src/tests/TestTicketMachine.java
@@ -3,12 +3,11 @@ package tests;
 import code.TicketMachine;
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 
 /*
  * assert methods
- *  assertEquals (message, expected, actual)	
+ *  assertEquals (message, expected, actual)
  *  assertEquals (expected, actual, delta)
  *  assertEquals (expected, actual)
  *  // many more versions of assertEquals(...)
@@ -25,12 +24,6 @@ public class TestTicketMachine {
 
 	TicketMachine tm = null;
 
-/*
-	@Before
-	public void setUp(){
-		tm = new TicketMachine(100);
-	}
-*/
 	@Test
 	public void testTicketMachine() {
 		tm = new TicketMachine(100);
@@ -45,36 +38,88 @@ public class TestTicketMachine {
 	}
 
 	@Test
-	public void testGetCurrentCollected(){
-		TicketMachine tm = new TicketMachine(100);
-		tm.currentCollected(25);
-		assertEquals(25, tm.getCurrentCollected());
-	//	tm.currentCollected(-1);
-	//	assertEquals("")
-	}
-
-	@Test
 	public void testCurrentCollected(){
 		TicketMachine tm = new TicketMachine(100);
 		tm.currentCollected(-1);
-		assertEquals();
+		assertEquals("Negative numbers cannot be inserted",0, tm.getCurrentCollected());
+		tm.currentCollected(100);
+		assertEquals("Inserting ticket money amount incorrect",100, tm.getCurrentCollected());
+		tm.currentCollected(50);
+		assertEquals("Adding money towards partially paid ticket incorrect",150, tm.getCurrentCollected());
 	}
 
 	@Test
 	public void testPrintTicket(){
 		TicketMachine tm = new TicketMachine(100);
+		tm.currentCollected(100);
 		String ticket = tm.printTicket();
-		assertEquals("This is your ticket", ticket);
+		assertEquals("This is your regular ticket", ticket);
+		tm = new TicketMachine(100);
+		tm.currentCollected(20);
+		assertEquals("Insufficient funds", tm.printTicket());
+	}
 
+	@Test
+	public void testRefundAmount(){
+		TicketMachine tm = new TicketMachine(100);
+		tm.currentCollected(125);
+		tm.printTicket();
+		assertEquals("Refund balance amount incorrect", 25, tm.getRefundAmount());
+	}
+
+	@Test
+	public void testAmountRequired(){
+		TicketMachine tm = new TicketMachine(100);
+		tm.currentCollected(25);
+		tm.printTicket();
+		assertEquals("Amount required to complete ticket incorrect", 75, tm.getAmountRequired());
+	}
+
+	@Test
+	public void testTotalMoney(){
+		TicketMachine tm = new TicketMachine(100);
+		for(int i = 0; i < 3; i++){
+			tm.currentCollected(100);
+			tm.printTicket();
+		}
+		for(int i = 0; i < 3; i++){
+			tm.currentCollected(200);
+		}
+		assertEquals("Total amount of money collected incorrect",900, tm.getTotalMoney());
+	}
+
+	@Test
+	public void testFirstClass(){
+		TicketMachine tm = new TicketMachine(100);
+		tm.currentCollected(200);
+		tm.firstClass();
+		assertEquals(0, tm.getCurrentCollected());
+		assertEquals(1, tm.getFCTicketCount());
 	}
 
 	@Test
 	public void testGetTicketCount(){
 		TicketMachine tm = new TicketMachine(100);
-		//tm.printTicket();
-		assertNotNull(tm.getTicketCount());
+		tm.currentCollected(100);
+		tm.printTicket();
+		tm.currentCollected(100);
+		tm.printTicket();
+		assertEquals("Number of tickets incorrect",2, tm.getGrandTicketCount());
 	}
 
-	//@Test
+	@Test
+	public void testMoneyReport(){
+		TicketMachine tm = new TicketMachine(100);
+		tm.currentCollected(100);
+		tm.printTicket();
+		tm.currentCollected(2000);
+		tm.firstClass();
+		tm.firstClass();
+		tm.firstClass();
+		tm.firstClass();
+		tm.printTicket();
+		assertEquals("Incorrect total tickets", 6, tm.getGrandTicketCount());
+		assertEquals("Incorrect total money", 1000, (tm.getFCTicketCount()*tm.getCost()*2 + (tm.getTicketCount()*tm.getCost())));
+	}
 
 }	// end - class TestTicketMachine
